a1c28c96b433cefac72a6bdbe40524d09cb7632d
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-06-18 23:23:27 +0900

    Initial commit

---
+++ src/main/java/personal/tutorial/springbootquerydsl/entity/Hello.java
@@ -0,0 +1,13 @@
+package personal.tutorial.springbootquerydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Hello {
+    @Id @GeneratedValue
+    private Long id;
+}
---
+++ src/test/java/personal/tutorial/springbootquerydsl/SpringBootQueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.tutorial.springbootquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQueryDslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
