6ec1ba0ccd2b21d6aea448771002d4272d91437a
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-06-18 23:39:42 +0900

    QueryDSL에 대한 기본적인 TestCase 생성

--- src/test/java/personal/tutorial/springbootquerydsl/SpringBootQueryDslApplicationTests.java
+++ src/test/java/personal/tutorial/springbootquerydsl/SpringBootQueryDslApplicationTests.java
@@ -1,13 +1,37 @@
 package personal.tutorial.springbootquerydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import personal.tutorial.springbootquerydsl.entity.Hello;
+import personal.tutorial.springbootquerydsl.entity.QHello;
+
 
 @SpringBootTest
+@Transactional
 class SpringBootQueryDslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        QHello qHello = QHello.hello;
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        Assertions.assertThat(result).isEqualTo(hello);
+        Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
